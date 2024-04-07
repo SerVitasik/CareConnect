c670e1b2f3ca694cc655f2529ac401e0d94cc89f
Vitalii Kulyk <vitaliy.kulik2313@gmail.com>
2024-04-07 12:09:08 +0300

    created main page

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,116 @@
 import * as functions from "./modules/functions.js";
 
 functions.isWebp();
+new Swiper(".gathering-swiper-1", {
+  // Optional parameters
+  // Navigation arrows
+  cssWidthAndHeight: true,
+  navigation: {
+    nextEl: ".gatherings__button-next-1",
+    prevEl: ".gatherings__button-prev-1",
+  },
+  slidesPerView: 2,
+  breakpoints: {
+    480: {
+      slidesPerView: 1,
+    },
+    992: {
+      slidesPerView: 2,
+    },
+  },
+});
 
+new Swiper(".gathering-swiper-2", {
+  // Optional parameters
+  // Navigation arrows
+  cssWidthAndHeight: true,
+  navigation: {
+    nextEl: ".gatherings__button-next-2",
+    prevEl: ".gatherings__button-prev-2",
+  },
+  slidesPerView: 3,
+  breakpoints: {
+    480: {
+      slidesPerView: 1,
+    },
+    992: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+new Swiper(".gathering-swiper-3", {
+  // Optional parameters
+  // Navigation arrows
+  cssWidthAndHeight: true,
+  navigation: {
+    nextEl: ".gatherings__button-next-3",
+    prevEl: ".gatherings__button-prev-3",
+  },
+  slidesPerView: 3,
+  breakpoints: {
+    480: {
+      slidesPerView: 1,
+    },
+    992: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+new Swiper(".gathering-swiper-3", {
+  // Optional parameters
+  // Navigation arrows
+  cssWidthAndHeight: true,
+  navigation: {
+    nextEl: ".gatherings__button-next-3",
+    prevEl: ".gatherings__button-prev-3",
+  },
+  slidesPerView: 3,
+  breakpoints: {
+    480: {
+      slidesPerView: 1,
+    },
+    992: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+new Swiper(".gathering-swiper-4", {
+  // Optional parameters
+  // Navigation arrows
+  cssWidthAndHeight: true,
+  navigation: {
+    nextEl: ".gatherings__button-next-4",
+    prevEl: ".gatherings__button-prev-4",
+  },
+  slidesPerView: 3,
+  breakpoints: {
+    480: {
+      slidesPerView: 1,
+    },
+    992: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+new Swiper(".gathering-swiper-5", {
+  // Optional parameters
+  // Navigation arrows
+  cssWidthAndHeight: true,
+  navigation: {
+    nextEl: ".gatherings__button-next-5",
+    prevEl: ".gatherings__button-prev-5",
+  },
+  slidesPerView: 3,
+  breakpoints: {
+    480: {
+      slidesPerView: 1,
+    },
+    992: {
+      slidesPerView: 2,
+    },
+  },
+});
